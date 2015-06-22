d046fface44ed862fdf14dd00dce26167f1d7ecf
Kira Cheung <killercentury@gmail.com>
2015-06-23 00:12:53 +1000

    add test for policy start date

--- test/e2e/find_your_car_spec.js
+++ test/e2e/find_your_car_spec.js
@@ -5,5 +5,7 @@ describe('find your car', function() {
     browser.get('/');
     element(by.id('productType1')).click();
     expect(element(by.model('productType')).getAttribute('value')).toEqual('COMPREHENSIVE');
+    element(by.id('policyStartDatePicker')).sendKeys('27/06/2015');;
+    expect(element(by.model('policyStartDate')).getAttribute('value')).toEqual('27/06/2015');
   });
 });
