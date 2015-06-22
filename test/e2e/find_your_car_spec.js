a27fa5fcf0931efae1b487775f631e2e9659422a
Kira Cheung <killercentury@gmail.com>
2015-06-23 00:21:46 +1000

    improve test case

--- test/e2e/find_your_car_spec.js
+++ test/e2e/find_your_car_spec.js
@@ -1,11 +1,13 @@
 'use strict';
 
-describe('find your car', function() {
-  it('should find the car', function() {
+describe('find your car', function () {
+  it('should find the car', function () {
     browser.get('/');
     element(by.id('productType1')).click();
     expect(element(by.model('productType')).getAttribute('value')).toEqual('COMPREHENSIVE');
-    element(by.id('policyStartDatePicker')).sendKeys('27/06/2015');;
-    expect(element(by.model('policyStartDate')).getAttribute('value')).toEqual('27/06/2015');
+    var date = new Date();
+    var formattedDate = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+    element(by.id('policyStartDatePicker')).sendKeys(formattedDate);
+    expect(element(by.model('policyStartDate')).getAttribute('value')).toEqual(formattedDate);
   });
 });
