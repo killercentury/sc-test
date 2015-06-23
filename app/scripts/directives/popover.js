c7cd64f805c74b2458aa5058c550aad3c43b452d
Kira Cheung <killercentury@gmail.com>
2015-06-24 04:03:25 +1000

    add popover for first two form inputs

---
+++ app/scripts/directives/popover.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name aamiApp.directive:popover
+ * @description
+ * # popover
+ */
+angular.module('aamiApp')
+  .directive('popover', function () {
+    return {
+      restrict: 'A',
+      link: function() {
+        angular.element('[data-toggle="popover"]').popover();
+      }
+    };
+  });
