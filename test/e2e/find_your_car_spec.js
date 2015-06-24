b66df9116251c126e48e58d8b47001ebb66655b1
Kira Cheung <killercentury@gmail.com>
2015-06-24 16:59:59 +1000

    add e2e test for error messages

--- test/e2e/find_your_car_spec.js
+++ test/e2e/find_your_car_spec.js
@@ -1,7 +1,7 @@
 'use strict';
 
-describe('find your car', function () {
-  it('should find the car', function () {
+describe('find your car', function() {
+  it('should find the car', function() {
     browser.get('/');
     element(by.id('productType1')).click();
     expect(element(by.model('productType')).getAttribute('value')).toEqual('COMPREHENSIVE');
@@ -9,5 +9,18 @@ describe('find your car', function () {
     var formattedDate = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
     element(by.id('policyStartDatePicker')).sendKeys(formattedDate);
     expect(element(by.model('policyStartDate')).getAttribute('value')).toEqual(formattedDate);
+  });
+
+  it('should see all error messages if only click the submit button', function() {
+    browser.get('/');
+    element(by.id('btn-find-car')).click();
+    expect(element(by.id('product-type-error')).getText()).toEqual('Please select one insurance type above.');
+    //expect(element(by.id('policy-start-date-error')).getText()).toEqual('Please select a policy start date from the calendar.');
+    expect(element(by.id('manufacture-year-error')).getText()).toEqual('Please select the year of manufacture.');
+    expect(element(by.id('car-make-error')).getText()).toEqual('Please select the car make.');
+    expect(element(by.id('car-model-error')).getText()).toEqual('Please select the car model.');
+    expect(element(by.id('transmission-type-error')).getText()).toEqual('Please select the transmission type.');
+    expect(element(by.id('cylinder-type-error')).getText()).toEqual('Please select the number of cylinders.');
+    expect(element(by.id('body-type-error')).getText()).toEqual('Please select the body type.');
   });
 });
